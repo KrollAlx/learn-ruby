a16c9d12ebd97061d2dfd62856559e13d938ea4d
Алексей <aleksey@MacBook-Air-Aleksej.local>
2022-01-18 00:44:09 +0300

    Initial commit

---
+++ db/migrate/20220117152338_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions do |t|
+      t.text :text
+      t.integer :right_answer
+      t.references :test, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
